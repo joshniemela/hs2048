34f5d4a7719e72672d18d761ec887acfa9a98a1e
Joshua Niemelä <josh@jniemela.dk>
2022-11-08 12:56:59 +0100

    cursed things

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Control.Concurrent.Async
+import Data.ByteString qualified as BS
 import Data.List
+import Network.Simple.TCP
 import System.IO (getChar, putStr)
 import System.Random
 import Prelude hiding (Down, Left, Right, putStr, sum)
@@ -15,6 +18,9 @@ type Tile = Maybe Int
 type Grid = [[Tile]]
 
 data Move = Up | Down | Left | Right
+
+-- boolean to tell if tcp should be used
+isTCP = True
 
 emptyGrid :: Grid
 emptyGrid = replicate 4 (replicate 4 Nothing)
@@ -145,10 +151,60 @@ loop (Just grid) = do
     'q' -> putStrLn "Bye!"
     _ -> loop (Just grid)
 
+-- Take grid which is a list of lists of tiles and convert it to a string
+encode :: Maybe Grid -> ByteString
+encode Nothing = "gameover"
+-- get the score of the grid and encode it as a string using recurrent score
+encode (Just grid) = show (scoreGrid grid) <> " " <> BS.intercalate " " (map encodeTile (concat grid))
+
+-- Encode a single tile
+encodeTile :: Tile -> ByteString
+encodeTile Nothing = "0"
+encodeTile (Just n) = show n
+
+socketLoop :: Maybe Grid -> Socket -> IO ()
+socketLoop Nothing _ = pass
+socketLoop (Just grid) socket = do
+  clearScreen
+  putStrLn "\n"
+  displayGrid grid
+  -- Get head or bytestring or get keyboard input
+
+  input <- race getChar (recv socket 1)
+
+  input
+    & either
+      ( \case
+          'w' -> move Up grid
+          'a' -> move Left grid
+          's' -> move Down grid
+          'd' -> move Right grid
+          'q' -> putStrLn "Bye!" >> return Nothing
+          _ -> return (Just grid)
+      )
+      ( \case
+          -- TCP input
+          Just "w" -> move Up grid
+          Just "a" -> move Left grid
+          Just "s" -> move Down grid
+          Just "d" -> move Right grid
+          Just "q" -> putStrLn "Bye!" >> return Nothing
+          _ -> return (Just grid)
+      )
+    -- Write new grid and return state on socket and convert grid to bytestring
+    >>= \newGrid -> send socket (encode newGrid) >> socketLoop newGrid socket
+
+-- Read port command line argument, if not present start normal loop
 main :: IO ()
 main = do
+  args <- getArgs
   hSetBuffering stdin NoBuffering
-  print ("Welcome to 2048! Press any key to start." :: String)
-  grid' <- placeRandomTile emptyGrid >>= placeRandomTile
-
-  loop (Just grid')
+  initialGrid <- placeRandomTile emptyGrid >>= placeRandomTile
+  case args of
+    ["-p", port] -> do
+      connect "localhost" port $ \(socket, _) -> do
+        send socket (encode (Just initialGrid))
+        socketLoop (Just initialGrid) socket
+    ["-h"] -> putStrLn "Options:\n -p <port> for game over network\n -h for help\n No arguments for local game"
+    ["-v"] -> putStrLn "Version 0.1" >> exitSuccess
+    _ -> loop (Just initialGrid)
